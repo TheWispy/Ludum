c408f1616831e45b8bf4e6405b386faf87595972
TheWispy <will0humph@gmail.com>
2017-07-29 16:43:03 +0100

    Enemy skeletonised

--- Ludum/Enemy.cs
+++ Ludum/Enemy.cs
@@ -13,6 +13,34 @@ namespace Ludum
         public int Health;
         public float MOVE_SPEED = 0.4f;
         public Animation EnemyAnimation;
+        public int Damage;
+        public int Value;
+        
+        public int Width
+        {
+            get { return EnemyAnimation.FrameWidth; }
+        }
 
+        public int Height
+        {
+            get { return EnemyAnimation.FrameHeight; }
+        }
+
+        float enemyMoveSpeed;
+
+        public void Initialize()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
     }
 }
